e3a19029eeeedf12c4fb5a9d23c2547f3d102e45
Erik Dan Tran <erikdantran99@gmail.com>
2021-10-13 18:22:13 -0500

    Completed schema, created data base and tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,29 @@
 CREATE DATABASE chat;
 
 USE chat;
+/* Describe your table here.*/
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE usernames (
+  id INT auto_increment,
+  username varchar(20),
+  CONSTRAINT usernames_pk PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id int auto_increment,
+  roomName varchar(30),
+  CONSTRAINT rooms_pk PRIMARY KEY (id)
+);
 
-
-
+CREATE TABLE messages (
+  id int auto_increment,
+  messages varchar(150),
+  usernameId int,
+  roomnameId int,
+  CONSTRAINT messages_pk PRIMARY KEY (id),
+  FOREIGN KEY (usernameId) REFERENCES usernames(id),
+  FOREIGN KEY (roomnameId) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
